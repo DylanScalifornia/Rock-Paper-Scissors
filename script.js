d98ecd179fbbd6f911adfed80ecf39ac26e175f5
DylanScalifornia <dylanscalifornia@gmail.com>
2024-05-25 14:45:48 -0700

    added math.Random so that the computer cycles through Rock, Paper, Scissors

---
+++ script.js
@@ -0,0 +1,7 @@
+function getComputerChoice(){
+    const arrOfChoices = ['Rock', 'Paper', 'Scissors']
+    const randomNum = Math.floor(Math.random() * 3)
+    console.log(arrOfChoices[randomNum])
+}
+
+getComputerChoice();
